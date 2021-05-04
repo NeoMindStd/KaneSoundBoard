4621d9281ef611d88b698947282c6d901d540c2a
도우진 <dwj1996@naver.com>
2021-05-05 03:23:30 +0900

    Implement example of app base architecture

--- app/src/main/java/com/neomind/kanesoundboard/MainActivity.kt
+++ app/src/main/java/com/neomind/kanesoundboard/MainActivity.kt
@@ -2,10 +2,35 @@ package com.neomind.kanesoundboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.neomind.kanesoundboard.databinding.ActivityMainBinding
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        viewModel.count.observe(this) {
+            binding.mainText.text = it.toString()
+        }
+
+        binding.mainDecreaseButton.setOnClickListener {
+            viewModel.decreaseCount()
+        }
+        binding.mainIncreaseButton.setOnClickListener {
+            viewModel.increaseCount()
+        }
     }
 }
